8dda6bbc358daaba72b0a1689de7f6f012fdd5e4
pamcgrath3 <149702705+pamcgrath3@users.noreply.github.com>
2024-02-18 23:07:02 -0600

    Update studious-invention.py
    added email feature which emails results

--- studious-invention.py
+++ studious-invention.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as bsoup
+import smtplib
 
 def main():
     with open("links.txt", "r") as file:
@@ -7,6 +8,17 @@ def main():
         print("Scraping " + str(len(lines)) + " items from links.txt:\n")
         for link in lines:
             check(link)
+        email = input("\nRecipient email: ")
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.starttls()
+        s.login("studiousinvention@gmail.com", "viesujixdvqczoja")
+        with open("draft.txt", "r", encoding = "utf-8") as draft:
+            body = draft.read()
+            subject = "Report for " + str(len(lines)) + " links"
+            full = f"Subject: {subject}\n\n{body}"
+            s.sendmail("studiousinvention@gmail.com", email, full.encode("utf-8"))
+            print("Email has been sent.")
+            s.quit()
   
 def check(link):
     request = Request(link, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'})
@@ -22,3 +34,8 @@ def check(link):
     endIndex2 = price.find("</span>")
     price = price[26:endIndex2]    
     print("Current price: " + price + "\n")
+
+    with open("draft.txt", "a", encoding="utf-8") as draft:
+            draft.write(link)
+            draft.write(title + "\n")
+            draft.write("Current price: " + price + "\n\n")
